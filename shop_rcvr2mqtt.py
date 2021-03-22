f3d6bfe58e72077a3b64e1c671eb1543c5a76529
Mark Kintzel <mkintzel@gmail.com>
2021-03-21 20:58:53 -0500

    Add button functionality back in for control

--- shop_rcvr2mqtt.py
+++ shop_rcvr2mqtt.py
@@ -23,8 +23,40 @@ import json
 # Import the mqtt module
 import paho.mqtt.client as mqtt
 
+#############################################################################
+# Initialize RFM69 Board
+#############################################################################
+# Button A
+btnA = DigitalInOut(board.D5)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D6)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Button C
+btnC = DigitalInOut(board.D12)
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
+
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
+
+# 128x32 OLED Display
+reset_pin = DigitalInOut(board.D4)
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+# Clear the display.
+display.fill(0)
+display.show()
+
+# Set up some variables to help with writing to the display
+display_width = display.width
+display_height = display.height
+display_line1 = 0
+display_line2 = 12
+display_line3 = 24
 
 # Define radio parameters.
 # Frequency of the raio in Mhz.  Must match
@@ -51,6 +83,7 @@ rfm69.destination = 5
 # initialize counter
 counter = 0
 ack_failed_counter = 0
+display_temps = False
 
 execution_loop = True
 while execution_loop:
@@ -61,9 +94,9 @@ while execution_loop:
     if packet is not None:
         # Display the packet text and rssi
         # Print out the raw bytes of the packet:
-#        print("Received (raw header):", [hex(x) for x in packet[0:4]])
-#        print("Received (ray payload):  {0}".format(packet[4:]))
-#        print("RSSI: {0}".format(rfm69.last_rssi))
+        # print("Received (raw header):", [hex(x) for x in packet[0:4]])
+        # print("Received (ray payload):  {0}".format(packet[4:]))
+        # print("RSSI: {0}".format(rfm69.last_rssi))
 
         packet_text = str(packet[4:], "utf-8")
 
@@ -90,14 +123,40 @@ while execution_loop:
 
         # Create a JSON string from the disctionary object
         sensor_json = json.dumps(sensor_dict)
-#        print(sensor_json)
-#        print("\n")
+        #print(sensor_json)
 
         # Publish MQTT message
         ourClient = mqtt.Client("kintzel_mqtt")  # Create a MQTT client object
         ourClient.connect("192.168.12.27", 1883)  # Connect to the test MQTT broker
         ourClient.publish("shop", sensor_json)
 
-        time.sleep(5)
+        # Check the board buttons
+        #   - If Button A has been pressed - display the temps
+        #   - If Button B or C has been pressed - clear the display
+        if not btnA.value:
+            # Button A Pressed
+            display_temps = True
+        elif not btnB.value:
+            # Button B Pressed
+            display_temps = False
+            display.text("B - Going Dark!", 10, display_line2, 1)
+            display.show()
+        elif not btnC.value:
+            # Button C Pressed
+            display_temps = False
+            display.text("C - Going Dark!", 10, display_line2, 1)
+            display.show()
+
+        if display_temps:
+            display.text("Front Room:  " + str(round(fDegF, 1)), 5, display_line1, 1)
+            display.text(" Back Room:  " + str(round(fBackDegF, 1)), 5, display_line2, 1)
+            display.text("      RSSI: " + str(rfm69.last_rssi), 5, display_line3, 1)
+            display.show()
+
+        time.sleep(10)
+
+        # Clear the display and lets do it again
+        display.fill(0)
+        display.show()
 
 
